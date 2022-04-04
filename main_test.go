ace36030fda46eec12ccb454a424b8a10dcf84ce
Tim Gossett <tim@mrgossett.com>
2022-04-04 17:36:13 -0400

    http.DefaultClient -> gumroad.client
    `doCheck` called `http.PostForm`, which delegates to
    `http.DefaultClient`, a generic HTTP client that has no specific
    security hardening. Instead, a package-local `*http.Client` is provided,
    using a new package-local `transport` as its `http.RoundTripper`.
    `transport` mimics `http.DefaultTransport`, except that
    `transport.TLSClientConfig` is a custom `*tls.Config` with an explicit
    certificate pool. The certificate pool is captured in `certPool`, which
    contains the system certificate root trust ring available at build time.
    
    `TestMITM` is added in `main_test.go` to validate intended behavior.

--- main_test.go
+++ main_test.go
@@ -1,10 +1,17 @@
 package gumroad
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"net/http/httputil"
+	"net/url"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 )
@@ -93,4 +100,110 @@ func TestErrors(t *testing.T) {
 			}
 		})
 	}
+}
+
+//go:generate go run generate_cert.go -host 127.0.0.1,::1 -ca self -duration 87600h -rsa-bits 4096    -out testdata/ca.pem
+//go:generate go run generate_cert.go -host 127.0.0.1,::1 -ca testdata/ca.pem       -ecdsa-curve P256 -out testdata/mitm.pem
+
+func TestMITM(t *testing.T) {
+	t.Parallel()
+	license := "DEADBEEF-CAFE1234-5678DEAD-BEEFCAFE"
+
+	// server will stand in for GumRoad, and assume that any license it sees is invalid
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		data, err := url.ParseQuery(string(body))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte(err.Error()))
+			return
+		}
+
+		resp := GumroadResponse{
+			Success: data.Get("license_key") == license,
+		}
+
+		switch resp.Success {
+		case true:
+			w.WriteHeader(http.StatusOK)
+		case false:
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			t.Errorf("error encoding response: %s", err)
+		}
+	}))
+	t.Cleanup(server.Close)
+
+	// add server's certificate to the trusted pool, as if it was signed by one the build system's
+	// trusted CAs (which is the case for GumRoad).
+	certPool.AddCert(server.Certificate())
+
+	err := doCheck(server.URL, "product", "fake-key")
+	if !strings.Contains(err.Error(), "invalid license") {
+		t.Fatalf("expected error to indicate that the license is invalid, but got: %s", err)
+	}
+
+	if err := doCheck(server.URL, "product", license); err != nil {
+		t.Fatalf("unexpected error when checking valid key: %s", err)
+	}
+
+	// mitm will act as a man-in-the-middle proxy, and will respond as if any license is valid
+	mitm := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(GumroadResponse{Success: true}); err != nil {
+			t.Errorf("error encoding response: %s", err)
+		}
+	}))
+
+	mitm.TLS = &tls.Config{Certificates: []tls.Certificate{parseCert(t, "mitm.pem")}}
+	mitm.Config.ErrorLog = log.New(ioutil.Discard, "", 0)
+	mitm.StartTLS()
+	t.Cleanup(mitm.Close)
+
+	err = doCheck(mitm.URL, "product", license)
+	if !strings.Contains(err.Error(), "failed check license") {
+		t.Fatalf("expected error to indicate that the license check failed, but got: %s", err)
+	}
+
+	// proxyServer will act as a legitimate reverse proxy
+	serverURL, _ := url.Parse(server.URL)
+	proxy := httputil.NewSingleHostReverseProxy(serverURL)
+	proxy.Transport = transport
+	proxyServer := httptest.NewTLSServer(proxy)
+	t.Cleanup(proxyServer.Close)
+
+	err = doCheck(proxyServer.URL, "product", "fake-key")
+	if !strings.Contains(err.Error(), "invalid license") {
+		t.Fatalf("expected error to indicate that the license is invalid, but got: %s", err)
+	}
+
+	if err := doCheck(proxyServer.URL, "product", license); err != nil {
+		t.Fatalf("unexpected error when checking valid key: %s", err)
+	}
+}
+
+func parseCert(t *testing.T, file string) tls.Certificate {
+	fp := filepath.Join("testdata", file)
+
+	bytes, err := ioutil.ReadFile(fp)
+	if err != nil {
+		t.Fatalf("cannot read %s: %s", fp, err)
+	}
+
+	cert, err := tls.X509KeyPair(bytes, bytes)
+	if err != nil {
+		t.Fatalf("error creating tls.Certificate: %s", err)
+	}
+
+	return cert
 }
